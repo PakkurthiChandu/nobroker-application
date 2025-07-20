9d6837a55d659b9df45796e8178a8532bc036b77
viswanath.yandapalli <viswanath.yandapalli.33.4@mountblue.tech>
2025-07-20 16:40:52 +0530

    implement amenities

--- src/main/java/com/noBroker/nobroker_application_project/controller/PropertyController.java
+++ src/main/java/com/noBroker/nobroker_application_project/controller/PropertyController.java
@@ -1,5 +1,6 @@
 package com.noBroker.nobroker_application_project.controller;
 
+import com.noBroker.nobroker_application_project.model.Amenity;
 import com.noBroker.nobroker_application_project.model.Property;
 import com.noBroker.nobroker_application_project.service.PropertyService;
 import org.springframework.stereotype.Controller;
@@ -24,6 +25,29 @@ public class PropertyController {
     public String addPropertyDetails(Property property){
        propertyService.saveProperty(property);
 
-        return "property-details";
+        return "amenities-details";
+    }
+
+    @PostMapping("/amenities")
+    public String addAmenities(Amenity amenity){
+        propertyService.saveAmenities(amenity);
+        System.out.println(amenity.getBathrooms());
+        System.out.println(amenity.getBalcony());
+        System.out.println(amenity.getWaterSupply());
+        System.out.println(amenity.getPetAllowed());
+        System.out.println(amenity.getGym());
+        System.out.println(amenity.getNonVeg());
+        System.out.println(amenity.getGatedSecurity());
+        System.out.println(amenity.getLift());
+        System.out.println(amenity.getGasPipeLine());
+        System.out.println(amenity.getPark());
+        System.out.println(amenity.getSecondaryNumber());
+        System.out.println(amenity.getHouseKeeping());
+        System.out.println(amenity.getInternetService());
+        System.out.println(amenity.getPowerBackUp());
+        System.out.println(amenity.getServentRoom());
+        System.out.println(amenity.getSwimmingPool());
+        System.out.println(amenity.getFireSafety());
+        return "amenities-details";
     }
 }
--- src/main/java/com/noBroker/nobroker_application_project/service/PropertyService.java
+++ src/main/java/com/noBroker/nobroker_application_project/service/PropertyService.java
@@ -1,5 +1,6 @@
 package com.noBroker.nobroker_application_project.service;
 
+import com.noBroker.nobroker_application_project.model.Amenity;
 import com.noBroker.nobroker_application_project.model.Property;
 import org.springframework.stereotype.Service;
 
@@ -9,5 +10,9 @@ public class PropertyService {
     Property property;
     public void  saveProperty(Property property) {
          this.property =property;
+    }
+
+    public void saveAmenities(Amenity amenity) {
+        property.setAmenity(amenity);
     }
 }
