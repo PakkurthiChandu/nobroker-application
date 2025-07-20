be7975ccca96fb2aa8700242ab87c7b976cdf7e4
PakkurthiChandu <21mh1a4241@acoe.edu.in>
2025-07-20 12:22:04 +0530

    first commit

---
+++ src/main/java/com/noBroker/nobroker_application_project/NobrokerApplicationProjectApplication.java
@@ -0,0 +1,13 @@
+package com.noBroker.nobroker_application_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NobrokerApplicationProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NobrokerApplicationProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/noBroker/nobroker_application_project/NobrokerApplicationProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noBroker.nobroker_application_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NobrokerApplicationProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
