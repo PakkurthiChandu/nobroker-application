60a4bd0c31e9c2dc5895c23370108cf7839dad77
PakkurthiChandu <21mh1a4241@acoe.edu.in>
2025-07-20 12:25:17 +0530

    Added Entities

---
+++ src/main/java/com/noBroker/nobroker_application_project/model/Photo.java
@@ -0,0 +1,24 @@
+package com.noBroker.nobroker_application_project.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "Photos")
+@Getter
+@Setter
+@ToString
+public class Photo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long photoId;
+
+    private String imageUrl;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "property_id")
+    private Property property;
+}
