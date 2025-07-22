7387002f7b8eb5af946b3bd1a88862551b2f4264
PakkurthiChandu <21mh1a4241@acoe.edu.in>
2025-07-22 10:11:20 +0530

    landing page and get all properties page

--- src/main/java/com/noBroker/nobroker_application_project/service/PropertyService.java
+++ src/main/java/com/noBroker/nobroker_application_project/service/PropertyService.java
@@ -12,8 +12,8 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-
 import java.io.IOException;
+
 import java.util.List;
 import java.util.Map;
 
