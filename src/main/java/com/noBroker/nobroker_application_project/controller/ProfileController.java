763e7776ddaf27826cb3ddc1af6017424a3f26ed
viswanath.yandapalli <viswanath.yandapalli.33.4@mountblue.tech>
2025-07-26 00:42:49 +0530

    map .env file

--- src/main/java/com/noBroker/nobroker_application_project/controller/ProfileController.java
+++ src/main/java/com/noBroker/nobroker_application_project/controller/ProfileController.java
@@ -15,10 +15,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
 
+import java.util.Optional;
 import java.util.Set;
 
 @Controller
@@ -58,17 +60,32 @@ public class ProfileController {
     }
 
     @PostMapping("/profile/update")
-    public String updateProfile(@ModelAttribute("user") User updatedUser) {
+    public String updateProfile(@ModelAttribute("user") User updatedUser, RedirectAttributes redirectAttributes) {
         User user = userRepository.findById(updatedUser.getUserId())
                 .orElseThrow(() -> new RuntimeException("User not found"));
+
+        Optional<User> userByEmail = userRepository.findByEmail(updatedUser.getEmail());
+        if (userByEmail.isPresent() && !userByEmail.get().getUserId().equals(user.getUserId())) {
+            redirectAttributes.addFlashAttribute("errorMessage", "Email is already in use.");
+            return "redirect:/profile/view/" + user.getUserId();
+        }
+
+        Optional<User> userByPhone = userRepository.findByMobilePhone(updatedUser.getMobilePhone());
+        if (userByPhone.isPresent() && !userByPhone.get().getUserId().equals(user.getUserId())) {
+            redirectAttributes.addFlashAttribute("errorMessage", "Mobile number is already in use.");
+            return "redirect:/profile/view/" + user.getUserId();
+        }
 
         user.setName(updatedUser.getName());
         user.setEmail(updatedUser.getEmail());
         user.setMobilePhone(updatedUser.getMobilePhone());
-
         userRepository.save(user);
+
+        redirectAttributes.addFlashAttribute("successMessage", "Profile updated successfully.");
         return "redirect:/profile/view/" + user.getUserId();
     }
+
+
 
     @GetMapping("/shortlisted-properties/{userId}")
     public String showShortlisted(@PathVariable("userId") Long userId,
